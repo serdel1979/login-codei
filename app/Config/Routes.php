78a786496ed5f207229bfc9ad14f57f9eb38a664
sergio <sdlbsso@gmail.com>
2023-12-26 11:44:32 -0300

    herencia de plantillas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -7,3 +7,4 @@ use CodeIgniter\Router\RouteCollection;
  */
 //$routes->get('/', 'Home::index');
 $routes->get('/', 'Login::index');
+$routes->get('dashboard', 'Dashboard::index');
---
+++ app/Views/Dashboard/desktop.php
@@ -0,0 +1,1 @@
+<?php $this->extend("Views/Dashboard/template"); ?>
